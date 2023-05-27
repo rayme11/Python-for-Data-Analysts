c724d2803ad078658b99e5e053eb7a810b4f7500
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-05-27 18:26:26 -0500

    Chapter 2 Final Commit

---
+++ Chapter_1_Variables_Data_Types/Strings.py
@@ -0,0 +1,12 @@
+name = "My name is 'Raymundo'"
+print(name)
+
+lastname = 'My name is \'Coolio '
+print(lastname)
+
+longStringIntoMultiLine = """Of course
+ your might want to do this as well in Python
+ so use the three double quotes
+ for multi lines """
+
+print(longStringIntoMultiLine)
