59020700292edbb98d00475b186b5ee82d486db5
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-05-29 14:10:15 -0500

    Chapter 3 Initial Commit

---
+++ Chapter_3_Complex_Data_Types/ComplexTypes_Examples.py
@@ -0,0 +1,11 @@
+
+print(len("abd"))
+print("hello", "my friend", sep =',')
+print("hello", "my friend", end ='*****')
+print('\n')
+print(min(1,2,3,4-5,7, 3000))
+print(max(1,2,3,4-5,7, 3000))
+
+print(len(123)) # shoud throw error
+print(len(12.33)) # should trhow error
+
